94922946c45eb3d10eb24c3e64d22f76e0445169
João Vitor Sobral <sobraljvss@gmail.com>
2026-06-17 13:40:30 -0300

    Refactored electromagnetic fields and optimized streaming

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 <ul>
 <li>Multiprocess parallelism</li>
 <li>Switch Maxwell-Boltzmann distribution with Bose-Einstein distribution</li>
-<li><del>Vector field for visualizing macroscopic velocities</del></li>
-<li><del>Bounce-back</del></li>
-<li>Numpy for matrices handling</li>
-<li>Laser cooling, evaporative cooling and electromagnetic fields</li>
+<li><del>Numpy for matrices handlingback</del></li>
+<li><del>Electromagnetic fields</del></li>
+<li>Evaporative cooling</li>
+<li>Laser cooling</li>
+<li>Liquid helium cooling</li>
 </ul>
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ t0 = datetime.datetime.now() # recording startup time for optimization compariso
 
 n = (100,100) # matrix coordinates
 
-# system's middle point and distances relative to it, that is, the trap's dimensions
+# system's middle point and distances relative to it, that is, the trap's dimensions (TODO: reinsert it into the code)
 mid = (n[0]//2,n[1]//2)
 var = (n[0]//10,n[1]//2)
 
@@ -25,93 +25,79 @@ SQUARED_SOUND_SPEED = 1/3 # standard for D2Q9
 elements = np.zeros((*n, 9)) # lattices with 9 distribution functions (f_i)
 elements[:,:,0] = 1 # all lattice start with only f_0 = 1
 updated_elements = np.zeros_like(elements) # for collisions
-is_fluid = np.full(n, True) # boolean matrix excluding trapped out particles
+is_fluid = np.full(n, True) # boolean matrix excluding trapped out particles (TODO: reinsert it into the code)
 
 density = np.ones(n)
-show_density = np.ones(n) # only for visualizing
 macroscopic_velocity = np.zeros((*n,2)) # in cartesian coordinates
 macro_norm = np.sum(macroscopic_velocity*macroscopic_velocity, axis=2) # matrix of |u|^2
 max_mach = (np.amax(macro_norm)/SQUARED_SOUND_SPEED)**(0.5) # maximum Mach number
+
+targets = np.zeros((*n,9,3), dtype='int64')
+electromagnetic_field = np.zeros_like(macroscopic_velocity)
+KQ = (.05, .1) # field intensity not regarding position, it has to depend on elements matrix size in order to maintain low Mach number
+
+# preparating matrices for streaming targeted lattices and electromagnetic fields
+for i in range(n[0]):
+    for j in range(n[1]):
+        # distances from fields
+        delta_left = j+1
+        delta_right = n[1]-j
+        delta_up = i+1
+        delta_down = n[0]-i
+
+        # field intensity regarding position
+        electromagnetic_field[i,j] = np.array((KQ[0]*(1/(delta_left**2) - 1/(delta_right**2)), KQ[1]*(1/(delta_down**2) - 1/(delta_up**2))))
+
+        for c in range(9):
+            target = np.array([i-VELOCITIES[c,1], j+VELOCITIES[c,0], c]) # next lattice, vertical has to be inverted because matrix indexing differs from cartesian indexing (above means line before)
+
+            # periodic boundary condition
+            if target[0] < 0: target[0] = n[0]-1
+            elif target[0] > n[0]-1: target[0] = 0
+            if target[1] < 0: target[1] = n[1]-1
+            elif target[1] > n[1]-1: target[1] = 0
+
+            targets[i,j,c] = target
 
 def collide():
     global updated_elements
     dotted_fv = np.einsum('ijk,lk->ijl', macroscopic_velocity, VELOCITIES) # matrix of c dot u
 
-    # maxwell-boltzmann distribution (to be replaced soon)
+    # Maxwell-Boltzmann distribution (TODO: replace it with Bose-Einstein distribution)
     taylor = 1 + dotted_fv/SQUARED_SOUND_SPEED + (dotted_fv**2)/(2*(SQUARED_SOUND_SPEED**2)) - (macro_norm/(2*SQUARED_SOUND_SPEED))[:,:, np.newaxis]
-    feq = WEIGHTS*taylor*density[:,:,np.newaxis]
+    feq = WEIGHTS*taylor*density[:,:,np.newaxis] # equilibrium function
     updated_elements = (1-OMEGA)*elements + OMEGA*feq # BGK approximation
 
-def stream(): # still have to vectorise this one
-    for i in range(n[0]):
-        for j in range(n[1]):
-            for c in range(9):
-                if is_fluid[i,j]: # only lattices with particles can stream to other lattices
-                    target = np.array([i-VELOCITIES[c,1], j+VELOCITIES[c,0]]) # next lattice, vertical has to be inverted because matrix indexing differs from cartesian indexing (above means line before)
+def stream(): 
+    # scattering new distributions to targeted lattices
+    elements[targets[...,0], targets[...,1], targets[...,2]] = updated_elements
 
-                    # periodic boundary condition (electromagnetic fields will impose its own boundary condition soon)
-                    if target[0] < 0: target[0] = n[0]-1
-                    elif target[0] > n[0]-1: target[0] = 0
-                    if target[1] < 0: target[1] = n[1]-1
-                    elif target[1] > n[1]-1: target[1] = 0
-
-                    elements[*target, c] = updated_elements[i,j,c]
-
-def simulate_electromagnetic_fields():
-    # TODO: replace sample values for acceleration and velocity threshold with the ones based in electromagnetic force and maximum allowed kinectic energy
-    for i in range(n[0]):
-        for j in range(n[1]):
-            # builds a rectangle trap in the middle, smooth the edges out so that it becomes ellipsoidal
-            if i in range(mid[0]-var[0],mid[0]+var[0]) and j in range(mid[1]-var[1],mid[1]+var[1]):
-                # macroscopic velocity towards middle point increases with distance from it
-                macroscopic_velocity[i,j] += [.005*((mid[1])-j)/(var[1]),-.01*((mid[0])-i)/(var[0])]
-            else:
-                # evaporative cooling: trapping high energy particles out
-                if np.mean(macro_norm) > .5 and macro_norm[i,j] >= 2*np.mean(macro_norm):
-                    macroscopic_velocity[i,j] = [0,0]
-                    is_fluid[i,j] = False
-                else:
-                    # attracts particles far from center with maximum intensity
-                    if i < mid[0]-var[0]:
-                        macroscopic_velocity[i,j] += [0,-.01]
-                    elif i > mid[0]+var[0]:
-                        macroscopic_velocity[i,j] += [0,.01]
-                    if j < mid[1]-var[1]:
-                        macroscopic_velocity[i,j] += [.005,0]
-                    elif j > mid[1]+var[1]:
-                        macroscopic_velocity[i,j] += [-.005,0]            
-    
 def measure():
     global density, show_density, macroscopic_velocity, macro_norm, max_mach, is_fluid
+
+    elements[~is_fluid] = [0,0,0,0,0,0,0,0,0] # removing particles through evaporative cooling
+
     density = np.sum(elements, axis=2) # summatory over f_i
-    show_density = np.sum(elements, axis=2)
+    macroscopic_velocity = (np.einsum('ijk,kl->ijl', elements, VELOCITIES) + electromagnetic_field)/density[:,:,np.newaxis] # summatory over f_i*c_i    
+    macroscopic_velocity = .995*macroscopic_velocity # laser cooling deacceleration (TODO: calculate arbitrary deacceleration)
 
-    # avoiding zero density on empty lattices so there is no division by zero; flow speed will be (0,0) anyway
-    density[~is_fluid] = 10^-5
-    show_density[~is_fluid] = 0
-
-    macroscopic_velocity = (np.einsum('ijk,kl->ijl', elements, VELOCITIES)/density[:,:,np.newaxis]) # summatory over f_i*c_i
-    simulate_electromagnetic_fields()
-    
-    macroscopic_velocity[~is_fluid] = [0,0]
     macro_norm = np.sum(macroscopic_velocity*macroscopic_velocity, axis=2) # matrix of |u|^2
     max_mach = (np.amax(macro_norm)/SQUARED_SOUND_SPEED)**(0.5) # maximum Mach number
 
-    is_fluid = np.full(n, True) # reset boolean matrix so empty lattices can be filled again
-
+    is_fluid.fill(True) # reset boolean matrix so empty lattices can be filled again
 
 # heatmap for density
 plt.ion()
 fig, ax = plt.subplots()
-img = plt.imshow(show_density, cmap='plasma')
+img = plt.imshow(density, cmap='plasma')
 plt.colorbar(img)
-    
+
 def iterate():
     for _ in range(ITERATIONS):
-        if _ % 10 == 0:
+        if (_+1) % 10 == 0:
             # useful simulation info displayed every 10 iterations
-            print(f'total density: {np.sum(show_density)}')
-            print(f'max squared flow speed: {np.amax(macro_norm)}')
+            print(f'total density: {np.sum(density)}')
+            print(f'max and mean squared flow speed: {np.amax(macro_norm)} | {np.mean(macro_norm)}')
             print(f'max Mach number: {max_mach}')
 
         collide()
@@ -119,11 +105,10 @@ def iterate():
         measure()
 
         # updating heatmap
-        img.set_data(show_density)
-        img.set_clim(vmin=0, vmax=np.amax(show_density))
+        img.set_data(density)
+        img.set_clim(vmin=0, vmax=np.amax(density))
         plt.draw()
         plt.pause(0.01)
-
 
 iterate()
 t1 = datetime.datetime.now() # recording ending time for optimization comparison
